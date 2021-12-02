4adb6bd6f42dddbd87ee78d0c10632a76480a9cc
Mark Wiese <mark.wiese@stud.hs-ruhrwest.de>
2021-12-02 21:15:59 +0100

    Extract average-generation to Method

--- src/main/java/org/hrw/stud/pomawies/aoc21/dayone/DayOne.java
+++ src/main/java/org/hrw/stud/pomawies/aoc21/dayone/DayOne.java
@@ -1,7 +1,9 @@
 package org.hrw.stud.pomawies.aoc21.dayone;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.hrw.stud.pomawies.aoc21.Util;
 import org.jetbrains.annotations.NotNull;
@@ -9,21 +11,25 @@ import org.jetbrains.annotations.NotNull;
 public class DayOne {
 	public static void main(String[] args) throws IOException {
 		final List<Integer> inputList = Util.streamFile("src/main/resources/dayOneInput", Integer::parseInt)
-			.toList();
-		final List<Integer> rollingAverages = IntStream.range(1, inputList.size() - 1)
-			.parallel()
-			.map(i -> IntStream.rangeClosed(i - 1, i + 1)
-				.parallel()
-				.map(inputList::get)
-				.sum()
-			).boxed()
-			.toList();
+			.collect(Collectors.toCollection(ArrayList::new));
+		final List<Integer> rollingAverages = rollingAverage(inputList, 3);
 		System.out.printf(
 			"Increases in depth for single measurements: %d%n" +
 			"Increases in depth for 3-measurement rolling averages: %d%n",
 			increasesInList(inputList),
 			increasesInList(rollingAverages)
 		);
+	}
+
+	private static List<Integer> rollingAverage(@NotNull List <@NotNull Integer> integers, int size) {
+		return IntStream.rangeClosed(0, integers.size() - size)
+			.parallel()
+			.map(i -> IntStream.range(i, i + size)
+				.map(integers::get)
+				.parallel()
+				.sum()
+			).boxed()
+			.toList();
 	}
 
 	private static int increasesInList(@NotNull List<@NotNull Integer> list) {
