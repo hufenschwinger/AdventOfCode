50fc5d1f053c82e544d33903a2054cd95294a8ef
Wiese, Mark (pomawies) <mark.wiese@stud.hs-ruhrwest.de>
2023-12-02 23:53:39 +0100

    1_1 working 1_2 working

--- src/main/java/org/hrw/stud/pomawies/aoc23/DayOne.java
+++ src/main/java/org/hrw/stud/pomawies/aoc23/DayOne.java
@@ -2,88 +2,82 @@ package org.hrw.stud.pomawies.aoc23;
 
 import java.io.IOException;
 import java.util.Comparator;
+import java.util.Map;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.hrw.stud.pomawies.aoc21.Util;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
 import io.reactivex.rxjava3.core.Flowable;
-import io.reactivex.rxjava3.core.Maybe;
+import io.reactivex.rxjava3.core.Single;
 
+@SuppressWarnings("java:S106")
 @NonNullByDefault
 public class DayOne {
-	private enum DIGITS {
-		ONE,
-		TWO,
-		THREE,
-		FOUR,
-		FIVE,
-		SIX,
-		SEVEN,
-		EIGHT,
-		NINE,
-		;
-		private static Flowable<DIGITS> entries = Flowable.fromArray(values()).cache();
-		private final String lcn = name().toLowerCase();
-		private final String num = String.valueOf(ordinal() + 1);
+	private static final Map<String, Integer> mappings = Map.ofEntries(
+		new ImmutablePair<>("1", 1),
+		new ImmutablePair<>("2", 2),
+		new ImmutablePair<>("3", 3),
+		new ImmutablePair<>("4", 4),
+		new ImmutablePair<>("5", 5),
+		new ImmutablePair<>("6", 6),
+		new ImmutablePair<>("7", 7),
+		new ImmutablePair<>("8", 8),
+		new ImmutablePair<>("9", 9),
+		new ImmutablePair<>("one", 1),
+		new ImmutablePair<>("two", 2),
+		new ImmutablePair<>("three", 3),
+		new ImmutablePair<>("four", 4),
+		new ImmutablePair<>("five", 5),
+		new ImmutablePair<>("six", 6),
+		new ImmutablePair<>("seven", 7),
+		new ImmutablePair<>("eight", 8),
+		new ImmutablePair<>("nine", 9)
+	);
 
-		public static String replaceAll(String input) {
-			String result = input;
-			while (true) {
-				final String copy = result;
-				final String replaced = entries
-											.flatMapMaybe(d -> Maybe.just(copy.indexOf(d.lcn))
-															  .filter(i -> i >= 0)
-															  .map(i -> Pair.of(d, i))
-											)
-											.sorted(Comparator.comparing(Pair::getRight))
-											.map(Pair::getLeft)
-											.firstElement()
-											.map(d -> copy.replaceAll(d.lcn, d.num))
-											.blockingGet();
-				if (replaced == null) {
-					break;
+	private static Flowable<Integer> allIndices(String input, String target) {
+		return Flowable.create(
+			emitter -> {
+				int index = input.indexOf(target);
+				while (index != -1) {
+					emitter.onNext(index);
+					index = input.indexOf(target, index + 1);
 				}
-				result = replaced;
-			}
-			return result;
-		}
+				emitter.onComplete();
+			},
+			BackpressureStrategy.BUFFER
+		);
+	}
+
+	public static int firstPlusLast(String input) {
+		Flowable<Integer> digits = Flowable.fromIterable(mappings.entrySet())
+									   .flatMap(e -> allIndices(input, e.getKey()).map(index -> Pair.of(index, e.getValue())))
+									   .sorted(Comparator.comparing(Pair::getLeft))
+									   .map(Pair::getRight)
+									   .cache();
+		return Single.merge(
+				digits.firstElement()
+					.map(i -> 10 * i)
+					.toSingle(),
+				digits.lastElement()
+					.toSingle()
+			)
+				   .reduce(0, Integer::sum)
+				   .blockingGet();
 	}
 
 	public static void main(String[] args) throws IOException {
-		Flowable.just(
-				"two1nine",
-				"eightwothree",
-				"abcone2threexyz",
-				"xtwone3four",
-				"4nineeightseven2",
-				"zoneight234",
-				"7pqrstsixteen"
-			)
-			.doOnNext(System.out::println)
-			.map(DIGITS::replaceAll)
-			.doOnNext(System.out::println)
-			.map(l -> l.replaceAll("\\D", ""))
-			.doOnNext(System.out::println)
-			.map(l -> l.charAt(0) + "" + l.charAt(l.length() - 1))
-			.doOnNext(System.out::println)
-			.map(d -> Integer.valueOf(d.charAt(0) + "" + d.charAt(d.length() - 1)))
-			.reduce(Integer::sum)
-			.doOnSuccess(System.out::println)
-			.blockingGet();
-
-
 		System.out.println(
 			Util.getLines("src/main/resources/23/1.txt")
-			.map(l -> l.replaceAll("\\D", ""))
-			.map(d -> Integer.valueOf(d.charAt(0) + "" + d.charAt(d.length()-1)))
-			.reduce(Integer::sum)
-			.blockingGet()
+				.map(l -> l.replaceAll("\\D", ""))
+				.map(d -> Integer.valueOf(d.charAt(0) + "" + d.charAt(d.length() - 1)))
+				.reduce(Integer::sum)
+				.blockingGet()
 		);
 
 		System.out.println(
 			Util.getLines("src/main/resources/23/1.txt")
-				.map(DIGITS::replaceAll)
-				.map(l -> l.replaceAll("\\D", ""))
-				.map(d -> Integer.valueOf(d.charAt(0) + "" + d.charAt(d.length()-1)))
+				.map(DayOne::firstPlusLast)
 				.reduce(Integer::sum)
 				.blockingGet()
 		);
