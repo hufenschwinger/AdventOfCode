d0a114885bc2140f8e8835b57535f64e09084076
Wiese, Mark (pomawies) <mark.wiese@stud.hs-ruhrwest.de>
2023-12-03 18:27:04 +0100

    3_2 working

--- src/main/java/org/hrw/stud/pomawies/aoc23/DayThree.java
+++ src/main/java/org/hrw/stud/pomawies/aoc23/DayThree.java
@@ -1,13 +1,14 @@
 package org.hrw.stud.pomawies.aoc23;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.TreeSet;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,7 +18,9 @@ import java.util.stream.Stream;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.hrw.stud.pomawies.aoc21.Util;
 
+@SuppressWarnings("java:S106")
 @NonNullByDefault
+
 public class DayThree {
 
 	private record Coordinates(int x, int y) implements Comparable<Coordinates> {
@@ -38,10 +41,17 @@ public class DayThree {
 		}
 	}
 
-	private record DetectedNumber(int beginColumn, int value, int length, Collection<Integer> indices) {
+	private record DetectedNumber(int y, int beginColumn, int value, int length, Collection<Integer> indices) {
 		private static final Pattern pattern = Pattern.compile("\\b\\d+");
 
-		public static List<DetectedNumber> allIn(String input) {
+		public Stream<Coordinates> toNeighbours() {
+			return indices()
+					   .stream()
+					   .map(x -> new Coordinates(x, y))
+					   .flatMap(Coordinates::toNeighbours);
+		}
+
+		public static List<DetectedNumber> allIn(int y, String input) {
 			List<DetectedNumber> numbers = new LinkedList<>();
 
 			Matcher matcher = pattern.matcher(input);
@@ -54,6 +64,7 @@ public class DayThree {
 											.toList();
 				numbers.add(
 					new DetectedNumber(
+						y,
 						matcher.start(),
 						Integer.parseInt(val),
 						len,
@@ -65,45 +76,81 @@ public class DayThree {
 		}
 	}
 
-	private record InputLine(Collection<DetectedNumber> numbers, Collection<Integer> signIndices) {
-		public static InputLine from(String line) {
+	private record InputLine(int y, Collection<DetectedNumber> numbers, Collection<Integer> signIndices, String line) {
+
+		public Stream<Coordinates> forChar(char target) {
+			return signIndices().stream()
+					   .filter(i -> line.charAt(i) == target)
+					   .map(x -> new Coordinates(x, y));
+		}
+
+		public Stream<Coordinates> allSigns() {
+			return signIndices().stream()
+					   .map(x -> new Coordinates(x, y));
+		}
+
+		public static InputLine from(int y, String line) {
 			return new InputLine(
-				DetectedNumber.allIn(line),
+				y,
+				DetectedNumber.allIn(y, line),
 				IntStream.range(0, line.length())
 					.filter(i -> Optional.of(line.charAt(i))
 									 .filter(Predicate.not(Character::isDigit))
 									 .filter(c -> c != '.')
 									 .isPresent())
 					.boxed()
-					.toList()
+					.toList(),
+				line
 			);
 		}
 	}
 
-	private static Stream<Coordinates> coordinatesForNumber(int y, DetectedNumber number) {
-		return number.indices()
-				   .stream()
-				   .map(x -> new Coordinates(x, y))
-				   .flatMap(Coordinates::toNeighbours)
-				   .distinct();
-	}
-
 	public static void main(String[] args) throws IOException {
-		List<InputLine> parsed = Util.streamFile("src/main/resources/23/3.txt")
-									 .map(InputLine::from)
-									 .collect(Collectors.toCollection(ArrayList::new));
-		final int size = parsed.size();
+		final var raw = Util.readFile("src/main/resources/23/3.txt");
+		final int size = raw.size();
 
-		Collection<Coordinates> allSigns = IntStream.range(0, size).boxed()
-											   .flatMap(y -> parsed.get(y).signIndices().stream()
-																	.map(x -> new Coordinates(x, y)))
-											   .collect(Collectors.toCollection(TreeSet::new));
+		final var parsed = IntStream.range(0, size)
+							   .mapToObj(y -> InputLine.from(y, raw.get(y)))
+							   .toList();
 
-		final long counter = IntStream.range(0, size).boxed()
-			.flatMap(y -> parsed.get(y).numbers.stream()
-							  .filter(num -> coordinatesForNumber(y, num).anyMatch(allSigns::contains)))
-			.mapToInt(DetectedNumber::value)
-			.sum();
+		final var allSigns = parsed.stream()
+								 .flatMap(InputLine::allSigns)
+								 .collect(Collectors.toCollection(TreeSet::new));
+
+		final var allNumbers = parsed.stream()
+								   .flatMap(il -> il.numbers()
+													  .stream())
+								   .collect(Collectors.toSet());
+
+		final long counter = allNumbers.stream()
+								 .filter(num -> num.toNeighbours()
+													.anyMatch(allSigns::contains))
+								 .mapToInt(DetectedNumber::value)
+								 .sum();
+
 		System.out.println("p1: " + counter);
+
+		final var allStarSigns = parsed.stream()
+									 .flatMap(inputLine -> inputLine.forChar('*'))
+									 .collect(Collectors.toCollection(TreeSet::new));
+
+		Map<Coordinates, List<DetectedNumber>> signsWithNeighbors = allStarSigns.stream()
+																		.collect(Collectors.toMap(
+																			Function.identity(),
+																			sc -> allNumbers.stream()
+																					  .filter(num ->
+																								  num.toNeighbours()
+																									  .toList()
+																									  .contains(sc))
+																					  .toList()
+																		));
+
+		final long sumOfGears = signsWithNeighbors.values()
+									.stream()
+									.filter(l -> l.size() == 2)
+									.mapToLong(l -> (long) l.get(0).value() * l.get(1).value())
+									.sum();
+
+		System.out.println("p2: " + sumOfGears);
 	}
 }
